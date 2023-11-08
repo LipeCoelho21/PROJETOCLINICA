20d85aa2bf4d0e73bd359a1603eb0705d2fcfdf9
LipeCoelho21 <filipecoelhoarce@gmail.com>
2023-11-07 20:03:40 -0400

    COMMIT 01

---
+++ src/dominio/Medico.java
@@ -0,0 +1,22 @@
+package dominio;
+
+import java.time.LocalDate;
+
+public class Medico extends BasePessoa {
+    
+    private String matricula;
+
+    public Medico(int codigo, String nome, String email, String rg, String cpf, LocalDate dataNascimento,
+            String matricula) {
+        super(codigo, nome, email, rg, cpf, dataNascimento);
+        this.matricula = matricula;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+}
---
+++ src/dominio/RegistroCRM.java
@@ -0,0 +1,21 @@
+package dominio;
+
+public class RegistroCRM extends BaseIdentificador{
+    
+    private String numero;
+
+    public RegistroCRM(int codigo, String numero) {
+        super(codigo);
+        this.numero = numero;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    
+}
